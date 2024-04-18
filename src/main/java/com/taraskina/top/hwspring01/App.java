23dfb6c57be3f21baaac1c55414ee954370c6f15
Sayana <s@mail.ru>
2024-04-18 22:33:38 +0700

    With java-code

--- src/main/java/com/taraskina/top/hwspring01/App.java
+++ src/main/java/com/taraskina/top/hwspring01/App.java
@@ -1,10 +1,10 @@
 package com.taraskina.top.hwspring01;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         Language day = context.getBean("onItalianBean", Language.class);
         day.printDay();
 
